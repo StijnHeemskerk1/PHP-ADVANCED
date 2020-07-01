b021d9ff5fc0b88e6d6dc23747d3d12699779f62
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2020-07-01 15:22:57 +0200

    2. taak 5 af

--- 2-Loops/taak05/index.php
+++ 2-Loops/taak05/index.php
@@ -8,7 +8,19 @@
 <body>
 
 	<?php 
-	
+
+	$klassen = array(
+		$student  = array("voornaam"=>"Jan", "achternaam"=>"Janssen", "klas"=>"9A", "Leeftijd"=>17, "Woonplaats"=>"Amstelveen")
+	);
+
+	echo "<table style='border: 1px solid black;'>";
+	foreach ($student as $key => $value) {
+		echo 	"<tr><td style='border: 1px solid black;'>$key</td> 
+				 <td style='border: 1px solid black;'>$value</td>
+				 </tr><br>";
+	}
+	echo "</table>";
+
 	?>
 
 </body>
