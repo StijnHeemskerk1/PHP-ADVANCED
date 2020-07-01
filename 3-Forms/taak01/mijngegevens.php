f6d5e000060553e15140be922e7bd82bce1a9775
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2020-07-01 15:38:39 +0200

    3. taak 1 af

---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php 
+		echo "Voornaam is: " . $_POST["voornaam"] . "<br>";
+		echo "Achternaam is: " . $_POST["achternaam"] . "<br>";
+		echo "Klas is: " . $_POST["klas"] . "<br>";
+		echo "Leeftijd is: " . $_POST["leeftijd"] . "<br>";
+		echo "Adres is: " . $_POST["adres"] . "<br>";
+		echo "Plaatsnaam is: " . $_POST["plaatsnaam"] . "<br>";
+		echo "Favoriete muziekband is: " . $_POST["favoriete_muziekband"] . "<br>";
+	?>
+
+</body>
+</html>
