d3dc95cb809ad11608c548c6e5ba2f22e17a153b
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2020-07-01 14:50:49 +0200

    2. taak 4 af

--- 2-Loops/taak04/loops-foreach-deel2.php
+++ 2-Loops/taak04/loops-foreach-deel2.php
@@ -6,9 +6,25 @@
 	<title></title>
 </head>
 <body>
-
-	<?php 
 	
+	<?php 
+		$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+		echo "<form><button>verzend</button></form>";
+
+
+
+		echo "<select>";
+		foreach($klassen as $value){
+			echo "<option>$value</option><br>";
+		}
+		echo "</select>";
+
+		
+		echo "<ul>";
+		foreach($klassen as $value1){
+			echo "<li>$value1</li><br>";
+		}
+		echo "</ul>";
 	?>
 
 </body>
