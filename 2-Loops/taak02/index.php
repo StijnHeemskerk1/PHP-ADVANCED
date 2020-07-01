b9131619f54585f6e4059a326c3f6bb84b99b11a
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2020-07-01 14:30:40 +0200

    2. taak 2 af

--- 2-Loops/taak02/index.php
+++ 2-Loops/taak02/index.php
@@ -8,7 +8,12 @@
 <body>
 
 	<?php 
-	
+		echo "<ul>";
+		for ($i=0; $i < 18; $i++){
+			echo "<li>Ik ben $i jaar, dus ik mag nog niet stemmen</li><br>";
+		}
+		echo "<li>Ik ben 18 jaar oud dus ik heb stemrecht!</li>";
+		echo "</ul>";
 	?>
 
 </body>
